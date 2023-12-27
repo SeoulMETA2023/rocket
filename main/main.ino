87a6e9e800118af97f43e60cb3c8b7d3c2269a4e
jeremy0917korea <51486099+jeremy0917korea@users.noreply.github.com>
2023-12-27 10:48:52 +0900

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,34 +1,42 @@
 #include <Wire.h>
 #include <Servo.h>
 #include <SoftwareSerial.h>
+
+
 #define LOOPTARGET 20
 #define mpu_add 0x68  // MPU6050 주소
 #define SERVO_PIN_1 9
 #define SERVO_PIN_2 10
 #define SERVO_PIN_3 11
 #define SERVO_PIN_4 12
+
+
 Servo servo1, servo2, servo3, servo4;
-SoftwareSerial AT09(2, 3);  // RX, TX
+SoftwareSerial bluetooth(2, 3);  // RX, TX
+
 
 class Kalman {
 public:
   double getKalman(double acc, double gyro, double dt) {
-    
     angle += dt * (gyro - bias);
-    
+
+
     P[0][0] += dt * (dt * P[1][1] - P[0][1] - P[1][0] + Q_angle);
     P[0][1] -= dt * P[1][1];
     P[1][0] -= dt * P[1][1];
     P[1][1] += Q_gyro * dt;
-    
+
+
     double S = P[0][0] + R_measure;
     K[0] = P[0][0] / S;
     K[1] = P[1][0] / S;
 
+
     double y = acc - angle;
     angle += K[0] * y;
     bias += K[1] * y;
-    
+
+
     double P_temp[2] = {P[0][0], P[0][1]};
     P[0][0] -= K[0] * P_temp[0];
     P[0][1] -= K[0] * P_temp[1];
@@ -36,6 +44,7 @@ public:
     P[1][1] -= K[1] * P_temp[1];
     return angle;
   }
+
 
   void init(double qAngle, double qGyro, double rMeasure) {
     Q_angle = qAngle;
@@ -48,50 +57,15 @@ public:
     P[1][0] = 0;
     P[1][1] = 0;
   }
-  double getVar(int num) {
-    switch (num) {
-      case 0:
-        return Q_angle;
-        break;
-      case 1:
-        return Q_gyro;
-        break;
-      case 2:
-        return R_measure;
-        break;
-    }
-  }
+ 
 private:
   double Q_angle, Q_gyro, R_measure;
   double angle, bias;
   double P[2][2], K[2];
 };
 
-/*class PIDcontrol {
-public:
-  void init(double kp, double ki, double kd) {
-    Kp = kp;
-    Ki = ki;
-    Kd = kd;
-    lastError = 0;
-    integral = 0;
-  }
-  double calculate(double setpoint, double currentValue, double dt) {
-    double error = setpoint - currentValue;
-    integral += error * dt;
-    double derivative = (error - lastError) / dt;
-    lastError = error;
-    return Kp * error + Ki * integral + Kd * derivative;
-  }
-private:
-
-  double Kp, Ki, Kd;  double lastError;
-  double integral;
-};*/
-
 
 Kalman kalmanRoll, kalmanPitch, kalmanYaw;
-//PIDController pidRoll, pidPitch, pidYaw;
 long ac_x, ac_y, ac_z, gy_x, gy_y, gy_z;
 double roll, pitch, yaw;
 double dt;
@@ -99,6 +73,8 @@ double targetRoll = 0.0;
 double targetPitch = 0.0;
 double targetYaw = 0.0;
 uint32_t pasttime;
+uint32_t last = 0;
+
 
 void rocketInit() {
   Serial.begin(9600);
@@ -120,7 +96,8 @@ void rocketInit() {
   servo4.attach(SERVO_PIN_4);
 }
 
-void readIMU(){
+
+void readIMU() {
   Wire.beginTransmission(mpu_add);
   Wire.write(0x3B);
   Wire.endTransmission(false);
@@ -137,28 +114,38 @@ void readIMU(){
   gy_z = Wire.read() << 8 | Wire.read();
 }
 
+
 void calcIMU() {
-  roll = atan2(ac_y, ac_z) * RAD_TO_DEG;
-  pitch = atan2(ac_x, sqrt(ac_y * ac_y + ac_z * ac_z)) * RAD_TO_DEG;
-  yaw = atan2(gy_y, gy_x) * RAD_TO_DEG;
+  double acc_roll = atan2(ac_y, ac_z) * RAD_TO_DEG;
+  double acc_pitch = atan2(ac_x, sqrt(ac_y * ac_y + ac_z * ac_z)) * RAD_TO_DEG;
+  double gyro_roll = gy_x / 131.0;
+  double gyro_pitch = gy_y / 131.0;
 
+
+  dt = (millis() - last) / 1000.0;
+  last = millis();
+
+
+  roll = kalmanRoll.getKalman(acc_roll, gyro_roll, dt);
+  pitch = kalmanPitch.getKalman(acc_pitch, gyro_pitch, dt);
+  yaw = kalmanYaw.getKalman(gy_z, gy_z, dt); 
 }
 
-/*void PID {
 
-}*/
-
-void printSerial(){
+void printSerial() {
   Serial.print(roll);
-  Serial.print("\t");
+  Serial.print(",");
   Serial.print(pitch);
-  Serial.print("\t");
-  Serial.println();
+  Serial.print(",");
+  Serial.println(yaw);
 }
-unsigned long last = 20;
+
+
+
 void setup() {
   rocketInit();
 }
+
 
 void loop() {
   if (millis() - pasttime > LOOPTARGET) {
@@ -171,6 +158,6 @@ void loop() {
     writeServo();
     teleRead();
     teleWrite();*/
-    last = millis();
+    pasttime = millis();
   }
 }
