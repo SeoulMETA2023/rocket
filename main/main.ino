549ad4ab66c60a065f92aab69a2d86b5db07a8a9
ydk0208 <ydk208@naver.com>
2023-12-28 00:01:40 +0900

    DK's skeleton code

--- main/main.ino
+++ main/main.ino
@@ -1,173 +1,239 @@
 #include <Wire.h>
-#include <Servo.h>
-#include <SoftwareSerial.h>
+
+#define mpu_add 0x68  //mpu6050 address
+#define kp 1
+#define ki 0.1
+#define kd 0.01
+#define threshold 20
+
+class kalman {
+
+  public :
+
+    double getkalman(double acc, double gyro, double dt) {
+
+      //project the state ahead
+
+      angle += dt * (gyro - bias) ;
+      //Project the error covariance ahead
+
+      P[0][0] += dt * (dt * P[1][1] - P[0][1] - P[1][0] + Q_angle) ;
+
+      P[0][1] -= dt * P[1][1] ;
+
+      P[1][0] -= dt * P[1][1] ;
+
+      P[1][1] += Q_gyro * dt ;
 
 
-#define LOOPTARGET 20
-#define mpu_add 0x68  // MPU6050 주소
-#define SERVO_PIN_1 9
-#define SERVO_PIN_2 10
-#define SERVO_PIN_3 11
-#define SERVO_PIN_4 12
+
+      //Compute the Kalman gain
+
+      double S = P[0][0] + R_measure ;
+
+      K[0] = P[0][0] / S ;
+
+      K[1] = P[1][0] / S ;
 
 
-Servo servo1, servo2, servo3, servo4;
-SoftwareSerial bluetooth(2, 3);  // RX, TX
+
+      //Update estimate with measurement z
+
+      double y = acc - angle ;
+
+      angle += K[0] * y ;
+
+      bias += K[1] * y ;
 
 
-class Kalman {
-public:
-  double getKalman(double acc, double gyro, double dt) {
-    angle += dt * (gyro - bias);
+
+      //Update the error covariance
+
+      double P_temp[2] = {P[0][0], P[0][1]} ;
+
+      P[0][0] -= K[0] * P_temp[0] ;
+
+      P[0][1] -= K[0] * P_temp[1] ;
+
+      P[1][0] -= K[1] * P_temp[0] ;
+
+      P[1][1] -= K[1] * P_temp[1] ;
 
 
-    P[0][0] += dt * (dt * P[1][1] - P[0][1] - P[1][0] + Q_angle);
-    P[0][1] -= dt * P[1][1];
-    P[1][0] -= dt * P[1][1];
-    P[1][1] += Q_gyro * dt;
+
+      return angle ;
+
+    } ;
+
+    void init(double angle, double gyro, double measure) {
+
+      Q_angle = angle ;
+
+      Q_gyro = gyro ;
+
+      R_measure = measure ;
 
 
-    double S = P[0][0] + R_measure;
-    K[0] = P[0][0] / S;
-    K[1] = P[1][0] / S;
+
+      angle = 0 ;
+
+      bias = 0 ;
 
 
-    double y = acc - angle;
-    angle += K[0] * y;
-    bias += K[1] * y;
+
+      P[0][0] = 0 ;
+
+      P[0][1] = 0 ;
+
+      P[1][0] = 0 ;
+
+      P[1][1] = 0 ;
+
+    } ;
+
+    double getvar(int num) {
+
+      switch (num) {
+
+        case 0 :
+
+          return Q_angle ;
+
+          break ;
+
+        case 1 :
+
+          return Q_gyro ;
+
+          break ;
+
+        case 2 :
+
+          return R_measure ;
+
+          break ;
+
+      }
+
+    } ;
+
+  private :
+
+    double Q_angle, Q_gyro, R_measure ;
+
+    double angle, bias ;
+
+    double P[2][2], K[2] ;
+
+} ;
 
 
-    double P_temp[2] = {P[0][0], P[0][1]};
-    P[0][0] -= K[0] * P_temp[0];
-    P[0][1] -= K[0] * P_temp[1];
-    P[1][0] -= K[1] * P_temp[0];
-    P[1][1] -= K[1] * P_temp[1];
-    return angle;
-  }
+kalman kal ;
+
+long ac_x, ac_y, ac_z, gy_x, gy_y, gy_z ;
+
+double pitch_gy, yaw_gy;
+double pitch_acc, yaw_acc;
+double pitch_theta, yaw_theta;
+double pitch_lastTheta, yaw_lastTheta;
+double pitch_integralTheta, yaw_integralTheta;
+double pitch_alpha, yaw_alpha;
+double pitch_PID, yaw_PID;
+
+double dt = 0.05;
+uint32_t lastTime ;
 
 
-  void init(double qAngle, double qGyro, double rMeasure) {
-    Q_angle = qAngle;
-    Q_gyro = qGyro;
-    R_measure = rMeasure;
-    angle = 0;
-    bias = 0;
-    P[0][0] = 0;
-    P[0][1] = 0;
-    P[1][0] = 0;
-    P[1][1] = 0;
-  }
- 
-private:
-  double Q_angle, Q_gyro, R_measure;
-  double angle, bias;
-  double P[2][2], K[2];
-};
-
-
-Kalman kalmanRoll, kalmanPitch, kalmanYaw;
-long ac_x, ac_y, ac_z, gy_x, gy_y, gy_z;
-double roll, pitch, yaw;
-double dt;
-double targetRoll = 0.0;
-double targetPitch = 0.0;
-double targetYaw = 0.0;
-uint32_t pasttime;
-uint32_t last = 0;
-
-
-void rocketInit() {
-  Serial.begin(9600);
-  bluetooth.begin(9600);
-  Wire.begin();
-  Wire.beginTransmission(mpu_add);
-  Wire.write(0x6B);
-  Wire.write(0);
-  Wire.endTransmission(true);
-  kalmanRoll.init(0.001, 0.002, 0.001);
-  kalmanPitch.init(0.001, 0.002, 0.001);
-  kalmanYaw.init(0.001, 0.002, 0.001);
-  targetRoll = 0;
-  targetPitch = 0;
-  targetYaw = 0;
-  servo1.attach(SERVO_PIN_1);
-  servo2.attach(SERVO_PIN_2);
-  servo3.attach(SERVO_PIN_3);
-  servo4.attach(SERVO_PIN_4);
-}
-
-
-void readIMU() {
-  Wire.beginTransmission(mpu_add);
-  Wire.write(0x3B);
-  Wire.endTransmission(false);
-  Wire.requestFrom(mpu_add, 6, true);
-  ac_x = Wire.read() << 8 | Wire.read();
-  ac_y = Wire.read() << 8 | Wire.read();
-  ac_z = Wire.read() << 8 | Wire.read();
-  Wire.beginTransmission(mpu_add);
-  Wire.write(0x43);
-  Wire.endTransmission(false);
-  Wire.requestFrom(mpu_add, 6, true);
-  gy_x = Wire.read() << 8 | Wire.read();
-  gy_y = Wire.read() << 8 | Wire.read();
-  gy_z = Wire.read() << 8 | Wire.read();
-}
-
-
-void calcIMU() {
-  double acc_roll = atan2(ac_y, ac_z) * RAD_TO_DEG;
-  double acc_pitch = atan2(ac_x, sqrt(ac_y * ac_y + ac_z * ac_z)) * RAD_TO_DEG;
-  double gyro_roll = gy_x / 131.0;
-  double gyro_pitch = gy_y / 131.0;
-
-
-  dt = (millis() - last) / 1000.0;
-  last = millis();
-
-
-  roll = kalmanRoll.getKalman(acc_roll, gyro_roll, dt);
-  pitch = kalmanPitch.getKalman(acc_pitch, gyro_pitch, dt);
-  yaw = kalmanYaw.getKalman(gy_z, gy_z, dt); 
-  /*roll = atan2(ac_y, ac_z) * RAD_TO_DEG;
-  pitch = atan2(-ac_x, sqrt(ac_y * ac_y + ac_z * ac_z)) * RAD_TO_DEG;
-  yaw = atan2(gy_y, gy_x) * RAD_TO_DEG;*/
-
-}
-
-
-void printSerial() {
-  /*Serial.print(roll);
-  Serial.print(",");
-  Serial.print(pitch);
-  Serial.print(yaw);
-  Serial.print(",");*/
-  Serial.print(roll);
-  Serial.print(",");
-  Serial.print(pitch);
-  Serial.print(",");
-  Serial.println(yaw);
-  
-}
-
-
+void readIMU();
+void calcIMU();
+void PID();
+void printSerial();
+void RCS();
+void tele();
 
 void setup() {
-  rocketInit();
+  Serial.begin(9600) ;
+  Wire.begin() ;
+  Wire.beginTransmission(mpu_add) ;
+  Wire.write(0x6B) ;
+  Wire.write(0) ;
+  Wire.endTransmission(true) ;
+  kal.init(0.001, 0.003, 0.03) ;  //init kalman filter
+  Serial.println() ;
+  Serial.print("parameter") ;
+  Serial.print("\t") ;
+  Serial.print(kal.getvar(0), 4) ;
+  Serial.print("\t") ;
+  Serial.print(kal.getvar(1), 4) ;
+  Serial.print("\t") ;
+  Serial.println(kal.getvar(2), 4) ;
 }
+
 
 
 void loop() {
-  if (millis() - pasttime > LOOPTARGET) {
+  if(millis()-lastTime >= dt*1000){
     readIMU();
     calcIMU();
     printSerial();
-    /*PID();
-    writeRCS();
-    readSensors();
-    writeServo();
-    teleRead();
-    teleWrite();*/
-    pasttime = millis();
+    PID();
+    RCS();
+    tele();
+
+    lastTime = millis();
   }
+}
+
+void readIMU(){
+  Wire.beginTransmission(mpu_add) ; //get acc data
+  Wire.write(0x3B) ;
+  Wire.endTransmission(false) ;
+  Wire.requestFrom(mpu_add, 6, true) ;
+  ac_x = Wire.read() << 8 | Wire.read() ;
+  ac_y = Wire.read() << 8 | Wire.read() ;
+  ac_z = Wire.read() << 8 | Wire.read() ;
+  Wire.beginTransmission(mpu_add) ; //get gyro data
+  Wire.write(0x43) ;
+  Wire.endTransmission(false) ;
+  Wire.requestFrom(mpu_add, 6, true) ;
+  gy_x = Wire.read() << 8 | Wire.read() ;
+  gy_y = Wire.read() << 8 | Wire.read() ;
+  gy_z = Wire.read() << 8 | Wire.read() ;
+}
+
+void calcIMU(){
+  pitch_acc = atan2(ac_x, ac_z) * 180 / PI ;  //acc data to degree data
+  pitch_gy = gy_y/131. ;
+  pitch_theta = kal.getkalman(pitch_acc,pitch_gy,dt);
+
+  yaw_acc = atan2(ac_y, ac_z) * 180 / PI ;  //acc data to degree data
+  yaw_gy = gy_y/131. ;
+  yaw_theta = kal.getkalman(yaw_acc,yaw_gy,dt);
+}
+
+void PID(){ 
+  pitch_integralTheta += dt * pitch_theta;
+  pitch_alpha = (pitch_theta-pitch_lastTheta)/dt;
+  pitch_PID = kp * pitch_theta + ki * pitch_integralTheta + kd * pitch_alpha;
+
+  yaw_integralTheta += dt * yaw_theta;
+  yaw_alpha = (yaw_theta-yaw_lastTheta)/dt;
+  yaw_PID = kp * yaw_theta + ki * yaw_integralTheta + kd * yaw_alpha;
+}
+
+void RCS(){
+  
+}
+
+void printSerial(){
+  Serial.print("kalman pitch") ;
+  Serial.print("\t") ;
+  Serial.print(pitch_theta) ;
+  Serial.print("\t kalman yaw") ;
+  Serial.print("\t") ;
+  Serial.println(yaw_theta) ;
+}
+
+void tele(){
+
 }
